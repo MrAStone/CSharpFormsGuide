883848094ca3ba850488514529abddeac2e58189
Al Stone <astone@heckgrammar.co.uk>
2024-11-25 20:15:07 +0000

    Commented Form 5 - Drawing basic graph

--- CSharpFormsGuide/Form5.Designer.cs
+++ CSharpFormsGuide/Form5.Designer.cs
@@ -29,9 +29,6 @@
         private void InitializeComponent()
         {
             panel1 = new Panel();
-            label1 = new Label();
-            label2 = new Label();
-            label3 = new Label();
             SuspendLayout();
             // 
             // panel1
@@ -42,53 +39,19 @@
             panel1.Size = new Size(518, 286);
             panel1.TabIndex = 0;
             // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Location = new Point(698, 393);
-            label1.Name = "label1";
-            label1.Size = new Size(38, 15);
-            label1.TabIndex = 1;
-            label1.Text = "label1";
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Location = new Point(698, 408);
-            label2.Name = "label2";
-            label2.Size = new Size(38, 15);
-            label2.TabIndex = 1;
-            label2.Text = "label1";
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Location = new Point(698, 426);
-            label3.Name = "label3";
-            label3.Size = new Size(38, 15);
-            label3.TabIndex = 1;
-            label3.Text = "label1";
-            // 
             // Form5
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(label3);
-            Controls.Add(label2);
-            Controls.Add(label1);
             Controls.Add(panel1);
             Name = "Form5";
             Text = "Form5";
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
 
         private Panel panel1;
-        private Label label1;
-        private Label label2;
-        private Label label3;
     }
 }
--- CSharpFormsGuide/Form5.cs
+++ CSharpFormsGuide/Form5.cs
@@ -14,82 +14,66 @@ namespace CSharpFormsGuide
     public partial class Form5 : Form
     {
         List<int> myData;
-        Graphics g;
-        int x, y;
-        int scale = 10;
-        int w, h;
+        int x, y,w,h;
         int count;
+        double scalerValue;
         Random rng;
         public Form5(List<int> data)
         {
-           
+
 
             InitializeComponent();
-            rng = new Random();
-            panel1.Width = this.Width - 75;
-            panel1.Height = this.Height - 75;
-            x =panel1.Location.X+ 10;
-            y = panel1.Location.Y+panel1.Height- 10;
-            
-            myData = data;
-            g = CreateGraphics();
-            w = this.Width;
-            h = this.Height;
-            panel1.Paint += new PaintEventHandler(Panel1_Paint);
-            this.SizeChanged += Form5_SizeChanged;
-            count = data.Count;
-            
+            rng = new Random(); // create a random number generator
+            panel1.Size = new Size(this.Width - 75, this.Height - 75); // set the panel to scale with the form
+            y = panel1.Location.Y + panel1.Height - 10; // set the y value to the bottom of the panel
+            myData = data; // set the data from the database
+            w = this.Width; // get the width of the form
+            h = this.Height; // get the height of the form
+            panel1.Paint += new PaintEventHandler(Panel1_Paint); // add the paint event to the panel
+            this.SizeChanged += Form5_SizeChanged; // add the size changed event to the form
+            scalerValue = myData.Max(); // get the maximum value of the data to be used in scaling hieght calculation (height/max value) * data value will scale the data to the panel height
+            count = data.Count; // get the number of data items to scale the width of the panel
+
         }
 
         private void Form5_SizeChanged(object? sender, EventArgs e)
         {
-            //if (this.Width < w || this.Height < h)
-            //{
-            //    scale -= 1;
-            //    w = this.Width;
-            //    h = this.Height;
-            //}
-            //else { scale += 1; }
-            panel1.Width = this.Width-75;
-            panel1.Height = this.Height-75;
-            panel1.Location = new Point(10,10);
-            y = panel1.Location.Y + panel1.Height - 10;
-            label1.Text = panel1.Height.ToString();
-            label2.Text = myData.Max().ToString();
+
+            panel1.Size = new Size(this.Width - 75, this.Height - 75); // set the panel to scale with the form
+            panel1.Location = new Point(10, 10); // set the panel to a fixed location
+            y = panel1.Location.Y + panel1.Height - 10; // set the y value to the bottom of the panel
+            panel1.Invalidate(); // redraw the panel
 
 
-            panel1.Invalidate();
-           
-            
         }
 
         private void Panel1_Paint(object? sender, PaintEventArgs e)
         {
-            Panel pp = sender as Panel;
-            Graphics g = e.Graphics;
-            Pen p = new Pen(Color.Red, 3);
-            Pen p2 = new Pen(Color.Green, 3);
-            int t = y;
-            int s = y;
-            x = panel1.Location.X + 10;
-            g.FillRectangle(new SolidBrush(Color.White), pp.DisplayRectangle);
-            for (int i = 0; i < myData.Count; i++)
+
+            Panel pp = panel1; // give panel a shorter name 
+            Graphics g = e.Graphics; // load graphics
+            Pen p1 = new Pen(Color.Red, 3); // pen for database data
+            Pen p2 = new Pen(Color.Green, 3); // pen for random data
+            int t = y;// start y value for the database data
+            int s = y; // start y value for the random data
+            x = 0; // start x value (0 is the left of the panel
+            g.FillRectangle(new SolidBrush(Color.Black), pp.DisplayRectangle); // fill the panel with white
+            for (int i = 0; i < myData.Count; i++) // loop through the demo data from the database
             {
-                                int d = myData[i];
-                //  MessageBox.Show(y.ToString());
-                d = (pp.Height / myData.Max()) * d;
-                g.DrawLine(p, x, t, x + pp.Width/count, y - (d));
-                //int r = rng.Next(1, pp.Height);
-                //g.DrawLine(p2, x, s, x + pp.Width / count, y - r);
-               // s = y - r;
-                x = x + pp.Width/count;
-                t = y - d;
+                double d = Convert.ToDouble(myData[i]); // get the value of data item
+                d = pp.Height / scalerValue * d; // set the scaling of the data (uses scaling calculation)
+                g.DrawLine(p1, x, t, x + pp.Width / count, Convert.ToInt32(y - (d))); // draw a line from x,y to the data point
+                int r = rng.Next(1, pp.Height); // generate a random number 
+                g.DrawLine(p2, x, s, x + pp.Width / count, y - r); // draw line for random number data
+                x = x + pp.Width / count; // set the next x point
+                t = Convert.ToInt32(y - d); // set the start y point for the next database item
+                s = y - r; // set the start y point for the next random data item
             }
 
         }
 
-       
 
-      
+
+
     }
 }
